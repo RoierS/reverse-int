1d85eff9437a1fa92378349d618672b7b9ca4407
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-01-07 18:33:02 +0100

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let nToArray = String(n).split('').map((n) => {return Number(n)});
+    let reverseArray = nToArray.reverse();
+    let result = parseFloat(reverseArray.join(''));
+    return result;
 }
